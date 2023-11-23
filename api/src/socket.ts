e362504b63e41a07c4c42c4f8c1e4fa91a176d76
A24-jo <bravo24ramosjose@gmail.com>
2023-11-22 20:59:04 -0500

    segundo avanmse

--- api/src/socket.ts
+++ api/src/socket.ts
@@ -4,7 +4,7 @@ import app from './app';
 
 const server = http.createServer(app);
 const io = new SocketServer(server,{
-    cors:{origin:'http://localhost:3000',methods:['get', 'post']},
+    cors:{origin:'https://mychat-1c7cbdf0n-a24-jo.vercel.app',methods:['get', 'post']},
 });
 
 
